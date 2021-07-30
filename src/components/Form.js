bc8cc8a6530d12d2b43ddf26b6ae7bdb943ab28c
James Richards <jd-app@AppAcademyAir.home>
2021-07-30 10:08:36 -0400

    add onBlur

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTextInput } from '../hooks/textInput';
 import { isEmail, isName } from '../utils/validations';
 import './Form.css';
@@ -11,30 +12,51 @@ const Form = () => {
     validation: isEmail,
   });
 
+  const [nameErrShow, setNameErrShow] = useState(false);
+  const [emailErrShow, setEmailErrShow] = useState(false);
+
+  const [disable, setDisable] = useState(true);
+
+  const handleBlur = (value, type) => {
+    value.length && type === 'name'
+      ? setNameErrShow(true)
+      : setNameErrShow(false);
+    value.length && type === 'email'
+      ? setEmailErrShow(true)
+      : setEmailErrShow(false);
+    isName(name).length || isEmail(email).length
+      ? setDisable(true)
+      : setDisable(false);
+  };
+
   return (
     <>
       <h1>Form Component</h1>
       <form>
         <label htmlFor='name'>
           <input
-            className={nameMessage(nameError)}
+            className={nameErrShow ? nameMessage(nameError) : ''}
             id='name'
             value={name}
             onChange={updateName}
+            onBlur={() => handleBlur(nameError, 'name')}
             placeholder='Name'
           />
-          <p className='form'>{nameError}</p>
+          <p className='form'>{nameErrShow && nameError}</p>
         </label>
         <label htmlFor='email'>
           <input
-            className={emailMessage(emailError)}
+            className={emailErrShow ? emailMessage(emailError) : ''}
             id='email'
             value={email}
             onChange={updateEmail}
+            onBlur={() => handleBlur(emailError, 'email')}
             placeholder='Email'
           />
-          <p className='form'>{emailError}</p>
+          <p className='form'>{emailErrShow && emailError}</p>
         </label>
+
+        <button disabled={disable}>Submit</button>
       </form>
     </>
   );
